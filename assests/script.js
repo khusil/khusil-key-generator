268da1b85c4778eadc846acd8920429f83eb10ff
khusil <khushilmanji@yahoo.com>
2023-03-12 15:10:02 -0400

    initial set up

---
+++ assests/script.js
@@ -0,0 +1,27 @@
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmLower;
+var confrimUpper;
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+  var upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S' , 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var characterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',];
+  
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);

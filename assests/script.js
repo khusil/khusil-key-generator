e45ece4c84fbb38e2c159b828b0460500bb3d7bd
khusil <khushilmanji@yahoo.com>
2023-03-12 15:48:14 -0400

    debugging code

--- assests/script.js
+++ assests/script.js
@@ -1,42 +1,42 @@
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-function generatePassword(){
+
+function generatePassword() {
   var upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S' , 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var characterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',];
-  var charactercount = promt ('how many characters you want between 8-128?');
+  var characterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+  var characterCount = prompt('How many characters do you want between 8-128?');
 
   var result = [];
-  var user = [];
-  var numbers = prompt ('how many numbers?');
-  var uppercase = prompt ('how many uppercase?');
-  var lowercase = prompt ('how many lowercase?');
-  var specialCharacters = prompt ('how many special characters?');
-  if (numbers){
+  var numbers = prompt('How many numbers?');
+  var uppercase = prompt('How many uppercase?');
+  var lowercase = prompt('How many lowercase?');
+  var specialCharacters = prompt('How many special characters?');
+
+  if (numbers) {
     result = result.concat(numberArray); 
   }
-  if (uppercase){
+  if (uppercase) {
     result = result.concat(upperArray);
   }
-  if (lowercase){
+  if (lowercase) {
     result = result.concat(lowerArray);
   }
-  if (characterArray){
+  if (specialCharacters) {
     result = result.concat(characterArray);
   }
-  
-   
+
+  var password = '';
+  for (let i = 8; i < characterCount; i++) {
+    password += result[Math.floor(Math.random() * result.length)];
+  }
+  return password;
 }
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

009b10860be06b137909de3c3fea85369864c2ae
khusil <khushilmanji@yahoo.com>
2023-03-13 20:46:31 -0400

    adding

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # khusil-key-generator
+## 
--- assests/script.js
+++ assests/script.js
@@ -1,12 +1,13 @@
 var generateBtn = document.querySelector("#generate");
-
+// create array
 function generatePassword() {
   var upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S' , 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var characterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
-  var characterCount = prompt('How many characters do you want between 8-128?');
-
+  var characterCount = prompt('How many characters do you want between 8-128?'); 
+ 
+// prompts to ask how many 
   var result = [];
   var numbers = prompt('How many numbers?');
   var uppercase = prompt('How many uppercase?');
@@ -25,8 +26,6 @@ function generatePassword() {
   if (specialCharacters) {
     result = result.concat(characterArray);
   }
-//check why characters are below 8 
-// try coding so password is not less than 8 characters by default
   var password = '';
   for (let i = 0; i < characterCount; i++) {
     password += result[Math.floor(Math.random() * result.length)];
